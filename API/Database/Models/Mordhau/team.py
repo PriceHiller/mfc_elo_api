e86563d87601b05988bfc6462386c72bb28bebed
Price Hiller <philler3138@gmail.com>
2021-03-22 08:35:39 -0500

    Nullables

--- API/Database/Models/Mordhau/team.py
+++ API/Database/Models/Mordhau/team.py
@@ -9,6 +9,6 @@ from API.Database.Models import AlcBase
 class Team(ModelBase, AlcBase):
     __tablename__ = "mfc_teams"
 
-    team_name = sqlalchemy.Column(sqlalchemy.String, unique=True)
-    elo = sqlalchemy.Column(sqlalchemy.Integer, index=True)
-    player = relationship("mfc_players")
+    team_name = sqlalchemy.Column(sqlalchemy.String, unique=True, nullable=False)
+    elo = sqlalchemy.Column(sqlalchemy.Integer, index=True, nullable=False)
+    player = relationship("mfc_players", cascade="all", default=-1)
