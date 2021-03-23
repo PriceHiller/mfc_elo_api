b52a47a2090743c6ec68ab7e8a64ebb3488994b2
Price Hiller <philler3138@gmail.com>
2021-03-23 00:43:48 -0500

    Schema/Crud Updates for Mordhau Player

--- API/Database/crud/Mordhau/player.py
+++ API/Database/crud/Mordhau/player.py
@@ -8,6 +8,7 @@ from API.Database.Models.Mordhau.player import Player as ModelPlayer
 from API.Database import BaseDB
 
 from API.Schemas.Mordhau.player import Player as SchemaPlayer
+from API.Schemas.Mordhau.player import PlayerID as SchemaPlayerID
 
 from .team import get_team_by_id
 
@@ -15,24 +16,11 @@ db = BaseDB.db
 
 
 async def create_player(player: SchemaPlayer) -> str:
-    if player.team_id:
-        team = await get_team_by_id(player.team_id)
-        if not team:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                                detail=f"Team with ID {player.team_id} not found")
-        player.team_id = str(team.id)
-        query = ModelPlayer.__table__.insert().values(
-            player_name=player.player_name,
-            playfab_id=player.playfab_id,
-            steam64=player.steam64,
-            team_id=player.team_id
-        )
-    else:
-        query: ModelPlayer.__table__.select = ModelPlayer.__table__.insert().values(
-            player_name=player.player_name,
-            playfab_id=player.playfab_id,
-            steam64=player.steam64,
-        )
+    query: ModelPlayer.__table__.select = ModelPlayer.__table__.insert().values(
+        player_name=player.player_name,
+        playfab_id=player.playfab_id,
+        steam64=player.steam64,
+    )
 
     try:
         return await db.execute(query)
@@ -48,14 +36,14 @@ async def delete_player(player_id):
     return await db.execute(query)
 
 
-async def get_player_by_name(player_name: str) -> SchemaPlayer:
+async def get_player_by_name(player_name: str) -> SchemaPlayerID:
     query: ModelPlayer.__table__.select = ModelPlayer.__table__.select().where(
         ModelPlayer.player_name == player_name
     )
 
-    if result := await db.fetch_one(query):
-        result: Record
-        return SchemaPlayer(**dict(result))
+    if result := dict(await db.fetch_one(query)):
+        result["id"] = str(result["id"])
+        return SchemaPlayerID(**result)
     else:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
@@ -63,14 +51,14 @@ async def get_player_by_name(player_name: str) -> SchemaPlayer:
         )
 
 
-async def get_player_by_id(id) -> SchemaPlayer:
+async def get_player_by_id(id) -> SchemaPlayerID:
     query: ModelPlayer.__table__.select = ModelPlayer.__table__.select().where(
         ModelPlayer.id == id
     )
 
-    if result := await db.fetch_one(query):
-        result: Record
-        return SchemaPlayer(**dict(result))
+    if result := dict(await db.fetch_one(query)):
+        result["id"] = str(result["id"])
+        return SchemaPlayerID(**result)
     else:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
@@ -78,7 +66,7 @@ async def get_player_by_id(id) -> SchemaPlayer:
         )
 
 
-async def get_players() -> list[ModelPlayer]:
+async def get_players() -> list[SchemaPlayerID]:
     query: ModelPlayer.__table__.select = ModelPlayer.__table__.select()
 
     if result := await db.fetch_all(query):
@@ -86,6 +74,6 @@ async def get_players() -> list[ModelPlayer]:
         for player in result:
             player = dict(player)
             player["id"] = str(player["id"])
-            players.append(player)
+            players.append(SchemaPlayerID(**player))
         return players
     return []
--- API/Schemas/Mordhau/player.py
+++ API/Schemas/Mordhau/player.py
@@ -3,13 +3,26 @@ from typing import Any
 
 from pydantic import BaseModel
 from pydantic import Field
+from pydantic import UUID4
 
 
 class BasePlayer(BaseModel):
     player_name: str
     playfab_id: str
-    team_id: Optional[str]
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "player_name": "Example",
+                "playfab_id": "abcd1234"
+            }
+        }
 
 
 class Player(BasePlayer):
     steam64: Optional[int]
+
+
+class PlayerID(Player):
+    id: str
+    team_id: Optional[str]
